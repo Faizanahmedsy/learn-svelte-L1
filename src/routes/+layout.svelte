9a8f571cba3fdbc4b12017f4144b14821dc73c49
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-31 11:58:24 +0530

    added tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+
+<!-- Slot is similar to children prop in react, it will render page.svelte -->
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,14 +20,24 @@
 
     let disabled = false
 
+    let     disClass = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+
+
+    if (disabled){
+        disClass = "text-white bg-red-400 dark:bg-red-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+    } else {
+        disClass= "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    }
+
+
 
 
 
 
 </script>
 
-<div class="center">
-<h1>Hello {name}</h1>
+<div class="flex flex-col justify-center items-center h-screen">
+<h1 class="text-4xl text-orange-300 font-bold p-4">Hello {name}</h1>
 <h2>I {isHired} work in {@html company}</h2>
 
 
@@ -36,17 +46,17 @@
 
 <button on:click={() => disabled = !disabled}>Toggle</button>
 <br />
-<button on:click={() => disabled = true}>Dont let me buy vimal</button>
+<button on:click={() => disabled = true} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Dont let me buy vimal</button>
 <br />
 
-<button on:click={() => disabled = false}>let me buy vimal</button>
+<button on:click={() => disabled = false} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">let me buy vimal</button>
 
 <br />
 
 
 
 
-<button {disabled} on:click={() => alert("Bolo zuba kesri")}>Buy vimal</button>
+<button {disabled} on:click={() => alert("Bolo zuba kesri")} class={disabled ? "text-white bg-zinc-400 dark:bg-zinc-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center" : "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"}>Buy vimal</button>
 
 <br />
 
@@ -55,18 +65,18 @@
 
 
 <style>
-    h1 {
+    /* h1 {
         color: goldenrod;
         font-family: "Comic Sans MS", cursive, sans-serif;
         font-size: 2rem;
-    }
+    } */
 
  
-    .center {
+    /* .center {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         height: 100vh;
-    }
+    } */
 </style>
