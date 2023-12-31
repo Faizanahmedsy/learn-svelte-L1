a3e7031a8f1908967c4d64114392198546732679
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-31 09:45:07 +0530

    learned svelte if else boook aka conditional rendering

---
+++ src/routes/conditionalrendering/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    let value = 0;
+</script>
+
+<section>
+
+<input type="number" on:change={
+
+    (event) => {
+        value = event.target.value;
+        if (value > 1000000) {
+        alert('Too big!');
+        }
+    }
+}>
+
+<h1>{value}</h1>
+
+
+
+{#if value%2 === 0}
+    <p>This is even number</p>
+{:else if value%2 != 0}
+    <p>Thats a odd number</p>
+{:else}
+    <p>Extrenal server error</p>
+{/if}
+
+
+</section>
+
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        height: 100vh;
+    }
+</style>
+
+
+
+
