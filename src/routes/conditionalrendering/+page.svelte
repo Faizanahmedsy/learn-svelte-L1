cc9f9730934424826698707021448f4dc5bf3782
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-31 09:57:23 +0530

    added some notes in conditional rendering section

--- src/routes/conditionalrendering/+page.svelte
+++ src/routes/conditionalrendering/+page.svelte
@@ -1,35 +1,45 @@
 <script>
-    let value = 0;
-</script>
+    //This are initial states
+    let value = 0
+    let name = ["Vimal", "Kumar", "Singh"]
 
-<section>
 
-<input type="number" on:change={
-
-    (event) => {
+    function handleChange (event) {
         value = event.target.value;
         if (value > 1000000) {
         alert('Too big!');
         }
     }
-}>
+</script>
 
+<section>
+<input type="number" on:change={handleChange}/>
 <h1>{value}</h1>
 
-
-
+<!-- conditional rendering by svelte special if else blocks -->
 {#if value%2 === 0}
     <p>This is even number</p>
-{:else if value%2 != 0}
+{:else if value % 2 !== 0}
     <p>Thats a odd number</p>
 {:else}
     <p>Extrenal server error</p>
 {/if}
 
 
+
+<!-- iterating over an array  -->
+<div>
+    {#each name as item, index}
+        <h2 >{index + 1} {item}</h2>
+    {/each}
+</div>
+
 </section>
 
 
+
+
+<!-- //This is the css for this page -->
 <style>
     section {
         display: flex;
