3ad930ef115a770a945dccf9dd850c5769f63a1b
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-31 09:11:34 +0530

    learning state updates

---
+++ src/components/Button.svelte
@@ -0,0 +1,1 @@
+<button>this is a sexy button</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import Button from "../components/Button.svelte"
+
+    let name = "Faizan"
+
+    let company = "<i>Google</i>"
+
+
+    let knowDsa = false
+ 
+    let isHired;
+
+    if(knowDsa) {
+         isHired = "do"
+    } else {
+         isHired = "do not"
+    }
+
+
+    let disabled = false
+
+
+</script>
+
+<div class="center">
+<h1>Hello {name}</h1>
+<h2>I {isHired} work in {@html company}</h2>
+
+
+<button on:click={() => disabled = !disabled}>Toggle</button>
+<br />
+<button on:click={() => disabled = true}>Dont let me buy vimal</button>
+<br />
+
+<button on:click={() => disabled = false}>let me buy vimal</button>
+
+<br />
+
+
+
+
+<button {disabled} on:click={() => alert("I am clicked")}>Buy vimal</button>
+
+<br />
+
+<Button />
+</div>
+
+
+<style>
+    h1 {
+        color: goldenrod;
+        font-family: "Comic Sans MS", cursive, sans-serif;
+        font-size: 2rem;
+    }
+
+
+    .center {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
