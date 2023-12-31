1de9a34778ced6a25a460992261a5c38e8aa0cf5
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-31 10:00:50 +0530

    learned how to iterate over an obejct

--- src/routes/conditionalrendering/+page.svelte
+++ src/routes/conditionalrendering/+page.svelte
@@ -4,6 +4,13 @@
     let name = ["Vimal", "Kumar", "Singh"]
 
 
+    let fullNames = [
+        {firstName: "Vimal", lastName: "Patel"},
+        {firstName: "Kumar", lastName: "Singh"},
+        {firstName: "Jaydeep", lastName: "Kumar"}
+    ]
+
+
     function handleChange (event) {
         value = event.target.value;
         if (value > 1000000) {
@@ -31,6 +38,12 @@
 <div>
     {#each name as item, index}
         <h2 >{index + 1} {item}</h2>
+    {/each}
+</div>
+
+<div>
+    {#each fullNames as item,index}
+    <h2>{index+ 1} {item.firstName} {item.lastName}</h2>
     {/each}
 </div>
 
