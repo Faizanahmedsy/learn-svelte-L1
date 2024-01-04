6529d0dc20f88c32827dbc37634b4178ddb44ba0
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-04 10:31:21 +0530

    refactor: indetened tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,11 +16,12 @@
     } else {
          isHired = "do not"
     }
+    
+let disabled = false
+    $:{ console.log("Hello idDisabled", disabled)}
 
 
-    let disabled = false
-
-    let     disClass = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    let  disClass = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 
 
     if (disabled){
